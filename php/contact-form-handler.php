9d1d42e244b58fb36dea2bd7ec399b98a1d8372e
Thomas Hansknecht <tfh0007.tigermail.auburn.edu>
2021-05-25 19:40:40 -0500

    I edited the contact form

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -1,7 +1,7 @@
 <?php
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$visitor_email = $_POST['email'];
+$first_name = $_POST['userFirst'];
+$last_name = $_POST['userLast'];
+$visitor_email = $_POST['userEmail'];
 $message = $_POST['message'];
 
 $email_from ='ghost24az@gmail.com';
@@ -9,7 +9,7 @@ $email_subject = "Someone just looked at your website";
 $email_body = "User First Name: $first_name \n\n
                 User Last Name: $last_name \n\n
                 User Email: $visitor_email\n\n
-                User Message $message\n\n";
+                User Message: $message\n\n";
 
 $to = "ghost34az@gmail.com";
 
@@ -17,7 +17,8 @@ $headers = "From: $email_from \r\n";
 
 $headers .= "Rely-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 
 header("Location: ../index.html");
 
