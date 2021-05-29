7b5c36e48c47fd9e44c479953803a7b278c89b8c
Thomas Hansknecht <tfh0007.tigermail.auburn.edu>
2021-05-28 21:11:30 -0500

    I added an intersection observer and start animations for page 2

--- javascript/observerPage2.js
+++ javascript/observerPage2.js
@@ -1,23 +1,24 @@
 //page1 will point to our .page1container which is the html class page1container
 // .page1container is the area where my website first becomes visable
 // We want to trigger play project videos when the videos become visible
-const page1 = document.querySelector(".page1container");
-var counter = 1;
-const options = {
+const page2 = document.querySelector(".page2container");
+var counter2 = 1;
+const options2 = {
 
     //root:null, means observing based on viewport
     //threshold: 0, means how much of the content has to be in view. 1 means 100% of the item has to be on the page
     //rootMargin: Using negative means the root margin is later on the page. Have to use pixels here
-    rootMargin: "0px"
+    rootMargin: "0px",
+    threshold: "0.1"
 
 };
 
-const page1observer = new IntersectionObserver(function(entries, obserever) {
+const page2observer = new IntersectionObserver(function(entries, obserever) {
 entries.forEach(entry => {
-    if (counter == 1 && entry.isIntersecting) {
+    if (counter2 == 1 && entry.isIntersecting) {
         console.log(entry),
         entry.target.classList.toggle("startAnimation");
-        counter++;
+        counter2++;
         }
     else {
         return;
@@ -26,6 +27,6 @@ entries.forEach(entry => {
     
 
 });
- }, options);
+ }, options2);
 
- page1observer.observe(page1);
+ page2observer.observe(page2);
